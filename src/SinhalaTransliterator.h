a5023a179b7c8dbc5a4d51bb061d4b2023075d91
thimiraonline <thimirathenuwara@gmail.com>
2026-08-08 12:05:34 +0530

    ✨: add SmartPhonetic input mode and corresponding transliteration logic

--- src/SinhalaTransliterator.h
+++ src/SinhalaTransliterator.h
@@ -2,6 +2,7 @@
 
 @interface SinhalaTransliterator : NSObject
 + (NSString *)transliteratePhonetic:(NSString *)input;
++ (NSString *)transliterateSmartPhonetic:(NSString *)input;
 + (NSString *)slsCharacterForInput:(NSString *)input shifted:(BOOL)shifted altGr:(BOOL)altGr;
 + (NSString *)normalizeSLSInputOrder:(NSString *)input;
 + (BOOL)isSinhalaInputUnit:(NSString *)input;
