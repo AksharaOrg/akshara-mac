bec1421fe254230e4027819b8430d5efe2a562c0
thimiraonline <thimirathenuwara@gmail.com>
2026-08-11 13:49:40 +0530

    🚑 update CFBundleShortVersionString and CFBundleVersion in Info.plist during packaging

--- script/package.sh
+++ script/package.sh
@@ -28,6 +28,8 @@ rm -rf "$PKG_ROOT" "$PKG_SCRIPTS" "$PKG_RESOURCES" "$PKG_DISTRIBUTION" "$COMPONE
 mkdir -p "$PKG_ROOT/Library/Input Methods" "$PKG_SCRIPTS" "$PKG_RESOURCES" "$DIST_DIR"
 
 cp -R "$APP" "$PKG_ROOT/Library/Input Methods/$APP_NAME.app"
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${VERSION#v}" "$PKG_ROOT/Library/Input Methods/$APP_NAME.app/Contents/Info.plist" || true
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion ${VERSION#v}" "$PKG_ROOT/Library/Input Methods/$APP_NAME.app/Contents/Info.plist" || true
 /usr/bin/xattr -cr "$PKG_ROOT/Library/Input Methods/$APP_NAME.app" 2>/dev/null || true
 /usr/bin/xattr -r -d com.apple.provenance "$PKG_ROOT/Library/Input Methods/$APP_NAME.app" 2>/dev/null || true
 /usr/bin/find "$PKG_ROOT" -name '._*' -delete
